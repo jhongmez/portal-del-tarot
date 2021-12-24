454a2e6dde2fc4e1007f7250bd44b42bc71046be
Jhon Gómez Marín <gmezjhon@gmail.com>
2021-12-24 00:12:43 -0500

    Welcome controller finished

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Header;
+use App\Service;
 use App\Testimonial;
 
 use Illuminate\Http\Request;
@@ -12,7 +13,10 @@ class WelcomeController extends Controller
     //
     public function list_data() {
         $headers      = Header::all();
+        $services     = Service::all();
         $testimonials = Testimonial::all();
-        return view('welcome')->with('headers', $headers)->with('testimonials', $testimonials);
+        return view('welcome')->with('headers', $headers)
+                              ->with('testimonials', $testimonials)
+                              ->with('services', $services);
     }
 }
