dec49616bf2b2b633700635328f422f6794b2b35
Jhon Gómez Marín <gmezjhon@gmail.com>
2021-12-30 09:27:57 -0500

    Error facades

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 // --- Solution: Specified kew  was too long: max  key length is 767 bytes
-use Illuminate\Support\facades\Schema;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
